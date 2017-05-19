eb0adbda6c54429a323907c1f493e4a44e6d24d3
Wil <wfkoch@gmail.com>
2017-05-19 19:41:33 -0400

    reorganizing

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='DroneParts',
+    name='droneparts',
     description="Parts library for developing drones",
-    version='0.1dev',
-    packages=['droneparts',],
+    version='0.2dev',
+    packages=find_packages(),
     install_requires=['solidpython'],
+    include_package_data = True,
     author="Wil Koch",
     license='Creative Commons Attribution-Noncommercial-Share Alike license'
 )
